b7dae8fc1b0c3bb2b815a4084c5763bcc7b8ce74
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-07-18 17:07:39 +0300

    review fixes

--- service/src/main/resources/bounds/getPersonLocations.sql
+++ service/src/main/resources/bounds/getPersonLocations.sql
@@ -7,5 +7,5 @@ FROM @cdmSchema.location_history lh
 	JOIN @cdmSchema.location l
 	  ON lh.location_id = l.location_id
 WHERE lh.domain_id = 'PERSON' AND lh.entity_id = @personId
-{@startDate != ''} ? {AND coalesce(end_date, '2099-10-25') >= '@startDate'}
-{@endDate != ''} ? {AND coalesce(start_date, '1900-01-01') <= '@endDate'}
+{@startDate != ''} ? {AND ISNULL(end_date, '2099-10-25') >= '@startDate'}
+{@endDate != ''} ? {AND ISNULL(start_date, '1900-01-01') <= '@endDate'}
